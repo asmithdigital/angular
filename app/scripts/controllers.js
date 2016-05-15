c34ed86979e8dfe6cc5508d1c40fe5e1e5dae946
Andrew Smith <andrew@asmithdigital.com>
2016-05-15 15:22:09 +0930

    Using $http to inject dishes from server, use baseURL constant for asset path in services

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -8,7 +8,13 @@ angular.module('confusionApp')
     $scope.filtText = '';
     $scope.showDetails = false;
 
-    $scope.dishes= menuFactory.getDishes();
+    $scope.dishes= [];
+    menuFactory.getDishes()
+      .then(
+      function(response) {
+        $scope.dishes = response.data;
+      }
+    );
 
 
     $scope.select = function(setTab) {
@@ -70,9 +76,14 @@ angular.module('confusionApp')
 
   .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
-    var dish= menuFactory.getDish(parseInt($stateParams.id,10));
-
-    $scope.dish = dish;
+    $scope.dish = {};
+    menuFactory.getDish(parseInt($stateParams.id,10))
+      .then(
+      function(response){
+        $scope.dish = response.data;
+        $scope.showDish=true;
+      }
+    );
 
   }])
 
@@ -97,9 +108,17 @@ angular.module('confusionApp')
 
   .controller('IndexController', ['$scope', 'corporateFactory', 'menuFactory', function($scope, corporateFactory, menuFactory){
 
-    $scope.leader = corporateFactory.getLeader(3);
+    $scope.feature = {};
 
-    $scope.feature = menuFactory.getDish(0);
+    menuFactory.getDish(0)
+      .then(
+      function(response){
+        $scope.feature = response.data;
+        $scope.showDish = true;
+      }
+    );
+
+    $scope.leader = corporateFactory.getLeader(3);
 
     $scope.promotion = menuFactory.getPromotion(0);
 
